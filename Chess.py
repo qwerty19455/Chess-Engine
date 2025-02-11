23b458fda62e82c92a13d7c18051135147a4660e
Bongo <soodshorya42@gmail.com>
2025-02-11 16:50:16 +0530

    Pieces moving according to their rules

--- Chess.py
+++ Chess.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.event
 import pygame.image
+from pygame import KEYDOWN
 
 import ChessEngine
 
@@ -9,28 +10,69 @@ Dimension = 8
 square_size = HEIGHT // Dimension
 Max_FPS = 15
 Images = {}
+square_selected = ()
+player_clicks = []
+
 
 
 def load_images():
     pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
-        Images[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png"), (square_size, square_size))
+        Images[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png"),
+                                               (square_size, square_size))
 
 
 def main():
+    global square_selected, square_selected, player_clicks, player_clicks
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     gs = ChessEngine.GameState()
+    valid_moves = gs.get_valid_moves() or []
+    move_made = False
     load_images()
     running = True
     while running:
         for e in pygame.event.get():
             if e.type == pygame.quit:
                 running = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()
+                col = location[0] // square_size
+                row = location[1] // square_size
+                if square_selected == (row, col):
+                    square_selected = ()
+                    player_clicks = []
+                else:
+                    square_selected = (row, col)
+                    player_clicks.append(square_selected)
+                if 2 == len(player_clicks):
+                    move = ChessEngine.Move(player_clicks[0], player_clicks[1], gs.board)
+                    print(move.getchessnotation())
+
+                    if move in valid_moves:
+                        gs.make_move(move)
+                        move_made = True
+                        square_selected = ()
+                        player_clicks = []
+                    else:
+                        player_clicks = [square_selected]
+
+            elif e.type == KEYDOWN:
+                 if e.key == pygame.K_z:
+                     gs.undo_move()
+                     move_made = True
+
+        if move_made:
+            valid_moves = gs.get_valid_moves() or []
+            move_made = False
+
+
+
         clock.tick(Max_FPS)
         drawGameState(screen, gs)
         pygame.display.flip()
+
 
 def drawGameState(screen, gs):
     drawBoard(screen)
@@ -41,8 +83,8 @@ def drawBoard(screen):
     colors = [pygame.Color("white"), pygame.Color("gray")]
     for r in range(Dimension):
         for c in range(Dimension):
-            color = colors[((r+c)%2)]
-            pygame.draw.rect(screen, color, pygame.Rect(c*square_size, r*square_size, square_size, square_size))
+            color = colors[((r + c) % 2)]
+            pygame.draw.rect(screen, color, pygame.Rect(c * square_size, r * square_size, square_size, square_size))
 
 
 def drawPieces(screen, board):
@@ -50,7 +92,8 @@ def drawPieces(screen, board):
         for c in range(Dimension):
             piece = board[r][c]
             if piece != "--":
-                screen.blit(Images[piece], pygame.Rect(c*square_size, r*square_size, square_size, square_size))
+                screen.blit(Images[piece], pygame.Rect(c * square_size, r * square_size, square_size, square_size))
+
 
 if __name__ == "__main__":
     main()
--- ChessEngine.py
+++ ChessEngine.py
@@ -9,11 +9,154 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
-            ]
-
-        self.whitetomove = True
+        ]
+        self.move_functions = {'p': self.get_pawn_moves, 'R': self.get_rook_moves, 'N': self.get_knight_moves,
+                               'B': self.get_bishop_moves, 'Q': self.get_queen_moves, 'K': self.get_king_moves}
+        self.white_to_move = True
         self.movelog = []
 
+    def make_move(self, move):
+        self.board[move.start_row][move.start_col] = "--"
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.movelog.append(move)
+        self.white_to_move = not self.white_to_move
 
-class ChessEngine:
-    pass
+    def undo_move(self):
+        if len(self.movelog) != 0:
+            move = self.movelog.pop()
+            self.board[move.start_row][move.start_col] = move.piece_moved
+            self.board[move.end_row][move.end_col] = move.piece_captured
+            self.white_to_move = not self.white_to_move
+
+    def get_valid_moves(self):
+        return self.get_all_possible_moves()
+
+    def get_all_possible_moves(self):
+        moves = []
+        for r in range(len(self.board)):  #number of rows
+            for c in range(len(self.board[r])):  #number of columns in given row
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.white_to_move) or (turn == 'b' and not self.white_to_move):
+                    piece = self.board[r][c][1]
+                    self.move_functions[piece](r, c, moves)
+        return moves
+
+    def get_pawn_moves(self, r, c, moves):
+        if self.white_to_move:
+            if self.board[r - 1][c] == "--":  #pawn advancing 1 square
+                moves.append(Move((r, c), (r - 1, c), self.board))
+                if r == 6 and self.board[r - 2][c] == "--":
+                    moves.append(Move((r, c), (r - 2, c), self.board))
+            if c - 1 >= 0:
+                if self.board[r - 1][c - 1][0] == 'b':  #enemy piece to capture to the left
+                    moves.append(Move((r, c), (r - 1, c - 1), self.board))
+            if c + 1 <= 7:
+                if self.board[r - 1][c + 1][0] == 'b':  #enemy piece to capture to the right
+                    moves.append(Move((r, c), (r - 1, c + 1), self.board))
+
+        else:
+            if self.board[r + 1][c] == "--":
+                moves.append(Move((r, c), (r + 1, c), self.board))
+                if r == 1 and self.board[r + 2][c] == "--":
+                    moves.append(Move((r, c), (r + 2, c), self.board))
+            if c - 1 >= 0:
+                if self.board[r + 1][c - 1][0] == 'w':
+                    moves.append(Move((r, c), (r + 1, c - 1), self.board))
+            if c + 1 <= 7:
+                if self.board[r + 1][c + 1][0] == 'w':
+                    moves.append(Move((r, c), (r + 1, c + 1), self.board))
+
+    def get_rook_moves(self, r, c, moves):
+        directions = ((-1, 0), (0, -1), (1, 0), (0, 1))  #up, left, down, right
+        enemy_color = "b" if self.white_to_move else "w"
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_col < 8:
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == "--":
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                else:
+                    break
+
+    def get_king_moves(self, r, c, moves):
+        directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        ally_color = "w" if self.white_to_move else "b"
+        for i in range(8):
+            end_row = r + directions[i][0]
+            end_col = c + directions[i][1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+
+
+
+    def get_knight_moves(self, r, c, moves):
+        knight_moves = ((-2, -1), (-2, 1), (-1, 2), (1, 2), (2, -1), (2, 1), (1, -2), (-1, -2))
+        ally_color = "w" if self.white_to_move else "b"
+        for m in knight_moves:
+            end_row = r + m[0]
+            end_col = c + m[1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+
+
+
+    def get_bishop_moves(self, r, c, moves):
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemy_color = "b" if self.white_to_move else "w"
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_col < 8:
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == "--":
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                else:
+                    break
+    def get_queen_moves(self, r, c, moves):
+        self.get_rook_moves(r, c, moves)
+        self.get_bishop_moves(r, c, moves)
+
+
+class Move():
+    rank_to_row = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    file_to_col = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    rows_to_ranks = {v: k for k, v in rank_to_row.items()}
+    col_to_file = {v: k for k, v in file_to_col.items()}
+
+    def __init__(self, start_sq, end_sq, board):
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+        self.move_ID = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
+        print(self.move_ID)
+
+        '''
+         Overriding the equals method
+        '''
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.move_ID == other.move_ID
+        return False
+
+    def getchessnotation(self):
+        return self.getrankfile(self.start_row, self.start_col) + self.getrankfile(self.end_row, self.end_col)
+
+    def getrankfile(self, r, c):
+        return self.col_to_file[c] + self.rows_to_ranks[r]
